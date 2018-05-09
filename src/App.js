6ecd6ea200fc56b8b2b7685debf81ed386830a05
davidkvara <davekvaratskhelia@gmail.com>
2018-05-09 19:44:24 +0400

    add prop-types; default props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "./assets/style.css";
 
-export default ({ name }) => (
+const App = ({ name }) => (
   <div className="container">
-    <h1 className="title">Ubralo React App</h1>
+    <h1 className="title">
+      Ubralo React App
+      <span title="PAWN" style={{ color: "black" }}>
+        &#9817;
+      </span>
+    </h1>
     <hr />
     <h2>Hello {name}!</h2>
   </div>
 );
+
+App.propTypes = {
+  name: PropTypes.string.isRequired
+};
+
+App.defaultProps = {
+  name: "World"
+};
+
+export default App;
