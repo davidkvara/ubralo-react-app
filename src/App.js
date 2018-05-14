82bf1c3e5d2b2db5dcb735d708cd0e6eac44087c
davidkvara <davekvaratskhelia@gmail.com>
2018-05-14 18:54:20 +0400

    mobx todo example

--- src/App.js
+++ src/App.js
@@ -4,16 +4,28 @@ import "./assets/style.css";
 
 @observer
 class App extends React.Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.store.addTodo(this.input.value);
+    this.input.value = "";
+  };
+
   render() {
     const { name, todos } = this.props.store;
 
     return (
       <div className="container">
-        <h1 className="title">Ubralo React App with Mobx</h1>
-        <h2>Hello {name}</h2>
-        <ul>{todos.map(todo => <li key={todo}>{todo}</li>)}</ul>
+        <React.Fragment>
+          <h1 className="title">Ubralo React App with Mobx</h1>
+          <h2>Hello {name}</h2>
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" ref={input => (this.input = input)} />
+          </form>
+          <ul>{todos.map(todo => <li key={todo.id}>{todo.item}</li>)}</ul>
+        </React.Fragment>
       </div>
     );
   }
 }
+
 export default App;
--- src/store.js
+++ src/store.js
@@ -1,8 +1,23 @@
-import { observable } from "mobx";
+import { observable, action } from "mobx";
+
+class Todo {
+  @observable item;
+  @observable id;
+  @observable completed;
+  constructor(value) {
+    this.item = value;
+    this.id = Date.now();
+    this.completed = false;
+  }
+}
 
 class AppStore {
   @observable name = "David";
-  @observable todos = ["one", "two", "three"];
+  @observable todos = [];
+  @action
+  addTodo(item) {
+    this.todos.push(new Todo(item));
+  }
 }
 
 const store = new AppStore();
